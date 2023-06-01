9fa06e9e070a4aa3bfd0b0183fed9ade81633f96
Eric Sommerhalder <eric.sommerhalder@unibas.ch>
2023-06-01 17:23:17 +0200

    cursor stuff

--- fourthplayground.js
+++ fourthplayground.js
@@ -1,15 +1,55 @@
-let player = document.getElementById("firstLottie");
+gsap.set(".ball", {xPercent: -50, yPercent: -50});
 
-player.addEventListener("ready", () => {
-  LottieInteractivity.create({
-        mode:"scroll",
-        player: "#firstLottie",
-        actions: [
-        {
-            visibility:[0, 1.0],
-            type: "seek",
-            frames: [0, 300],
-        },
-        ]
-      });
+const ball = document.querySelector(".ball");
+const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+const mouse = { x: pos.x, y: pos.y };
+const speed = 0.2;
+
+const xSet = gsap.quickSetter(ball, "x", "px");
+const ySet = gsap.quickSetter(ball, "y", "px");
+
+window.addEventListener("mousemove", e => {    
+  mouse.x = e.x;
+  mouse.y = e.y;  
 });
+
+gsap.ticker.add(() => {
+  
+  const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio()); 
+  
+  pos.x += (mouse.x - pos.x) * dt;
+  pos.y += (mouse.y - pos.y) * dt;
+  xSet(pos.x);
+  ySet(pos.y);
+});
+
+window.addEventListener("click", e => {
+  scale.play(0);
+  setTimeout(() => {
+    scale.reverse();
+  }, 200);
+});
+
+let scale = gsap.timeline({ paused: true })
+  .to(".ball", {
+    scale: 2,
+    transformOrigin: 'center',
+  });
+
+  let rotation = gsap.timeline({ paused: true })
+  .to(".ball", {
+    rotation: 45,
+    transformOrigin: 'center',
+    duration: 0.2,
+  });
+
+  let clickables = document.querySelectorAll('.clickable');
+  clickables.forEach(clickable => {
+    clickable.addEventListener("mouseover", () => {
+      rotation.play();
+    });
+  
+    clickable.addEventListener("mouseout", () => {
+      rotation.reverse();
+    });
+  });
--- src/sommerhalder-websolutions.js
+++ src/sommerhalder-websolutions.js
@@ -392,9 +392,66 @@ LottieInteractivity.create({
   mode:"scroll",
   actions: [
       {
-          visibility: [0, 1.0],
+          visibility: [0.2, 0.7],
           type: 'seek',
-          frames: [0, 195],
+          frames: [0, 180],
       },
   ]
 });
+
+/** cursor stuff */
+gsap.set(".cursor", {xPercent: -50, yPercent: -50});
+
+const cursor = document.querySelector(".cursor");
+const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+const mouse = { x: pos.x, y: pos.y };
+const speed = 0.2;
+
+const xSet = gsap.quickSetter(cursor, "x", "px");
+const ySet = gsap.quickSetter(cursor, "y", "px");
+
+window.addEventListener("mousemove", e => {    
+  mouse.x = e.x;
+  mouse.y = e.y;  
+});
+
+gsap.ticker.add(() => {
+  
+  const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio()); 
+  
+  pos.x += (mouse.x - pos.x) * dt;
+  pos.y += (mouse.y - pos.y) * dt;
+  xSet(pos.x);
+  ySet(pos.y);
+});
+
+window.addEventListener("click", e => {
+  scale.play(0);
+  setTimeout(() => {
+    scale.reverse();
+  }, 200);
+});
+
+let scale = gsap.timeline({ paused: true })
+  .to(".cursor", {
+    scale: 2,
+    transformOrigin: 'center',
+  });
+
+  let rotation = gsap.timeline({ paused: true })
+  .to(".cursor", {
+    rotation: 45,
+    transformOrigin: 'center',
+    duration: 0.2,
+  });
+
+  let clickables = document.querySelectorAll('.clickable');
+  clickables.forEach(clickable => {
+    clickable.addEventListener("mouseover", () => {
+      rotation.play();
+    });
+  
+    clickable.addEventListener("mouseout", () => {
+      rotation.reverse();
+    });
+  });
